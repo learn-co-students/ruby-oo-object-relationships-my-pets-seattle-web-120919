b745266b9b1fa8b782606d31647d60d765343a3a
github username <github email address>
2019-12-16 00:19:45 -0800

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
+#A Dog and a Cat are initialized with a name and an Owner object
+
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner) #can change owner, can't change name 
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+  end
+
+  # def mood=(mood)
+  #   @mood = mood
+  # end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,77 @@
+#An Owner will know about all their pets, be able to buy a pet, 
+#change a pet's mood through walking or feeding it, and sell all of their pets
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :dog, :cat, :mood
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self
+    @species = 'human'
+    Dog.all 
+    Cat.all 
+  end
+
+  def say_species # can say its species
+    "I am a #{@species}."
+  end 
+
+  def self.all # Returns all instances of Owner that have been created
+    @@all 
+  end
+
+  def self.count #returns the # of owners that have been created
+    @@all.count
+  end
+
+  def self.reset_all #reset the owners that have been created
+    @@all = []
+  end
+
+  # ASSOCIATIONS.  Owner instance methods. 
+  # returns a collection of all the cats that belong to the owner
+  def cats
+    Cat.all.select {|cats| cats.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|cats| cats.owner == self}
+  end 
+  
+  # can buy a cat that is an instance of the Cat class. knows about its cats
+  # accepts name and checks against Cat class 
+  # Make a new instance of the pet with the name, initializing it with that name 
+  # and the owner who is purchasing it.
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+  end 
+
+  # walks the dogs which makes the dogs' moods happy
+  def walk_dogs
+    Dog.self.mood = "happy"
+  end
+
+  def feed_cats
+    Cat.self.mood = "happy"
+  end 
+
+  # can sell all its pets, which makes them nervous
+  # can sell all its pets, which leaves them without an owner
+  def sell_pets
+  # iterate through all pets (cats + dogs)
+  pets = Cat.all + Dog.all 
+  pets.each {|pet| pet.mood = "nervous"}
+  pets.each {|pet| pet.owner = nil }
+  end 
+      
+  # can list off its pets
+  def list_pets
+    "I have #{@owner.dog.count} dog(s), and #{@owner.cat.count} cat(s)."
+  end
 end
